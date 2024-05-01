54d7e9d427a27fca1d0d85eb74572afb021279b1
Shashikant-Chaudhary <168453811+Shashikant-Chaudhary@users.noreply.github.com>
2024-05-01 22:42:53 +0530

    Tic-Tac-Toe Game project

---
+++ index.js
@@ -0,0 +1,76 @@
+let boxes = document.querySelectorAll(".box");
+let reset = document.querySelector(".Restart");
+let newGamebutton = document.querySelector(".new-button");
+let mesgContainer = document.querySelector(".mesgContainer");
+let msg = document.querySelector(".msg");
+let turno = true;
+
+let winPattern = [
+    [0,1,2],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],  
+];
+boxes.forEach((box) => {
+    box.addEventListener("click",() => {
+    if(turno === true)
+    {
+        box.innerText = "O";
+        turno = false;
+    }
+    else
+      { box.innerText = "X";
+       turno = true;}
+       box.disabled = true;
+       checkWinner();
+   });
+});
+
+const resetGame = () => {
+    turno = true;
+    enableBoxes(); 
+    mesgContainer.classList.add("hide");  
+};
+
+const disableBoxes = () => {
+    for(let box of boxes)
+    { box.disabled = true; }
+};
+
+const enableBoxes = () => {
+    for(let box of boxes)
+    {box.disabled = false;
+    box.innerText = "";}
+};
+
+
+const showWinner = (winner) => {
+     msg.innerText = `Winner is ${winner}`;
+     mesgContainer.classList.remove("hide");
+     disableBoxes();
+};
+
+const checkWinner = () => {
+    for(let pattern of winPattern)
+    // {console.log(pattern[0],pattern[1],pattern[2]);
+    // console.log(boxes[pattern[0]].innerText, boxes[pattern[1]].innerText, boxes[pattern[2]].innerText);
+   {
+    let pos1val = boxes[pattern[0]].innerText;
+    let pos2val = boxes[pattern[1]].innerText;
+    let pos3val = boxes[pattern[2]].innerText;
+
+    if(pos1val !="" && pos2val != "" && pos3val !="")
+    {
+        if(pos1val === pos2val && pos2val === pos3val)
+         {showWinner(pos1val);
+         return true;}
+    }
+   }
+
+};
+newGamebutton.addEventListener("click",resetGame);
+reset.addEventListener("click",resetGame);
